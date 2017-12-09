104163c0e073f66c688c36830cdef5a0f2331c47
Daniel Acquesta <danielacquesta@sbcglobal.net>
2017-12-09 15:32:04 -0800

    CRD routes working

--- sleeps/models.js
+++ sleeps/models.js
@@ -5,6 +5,7 @@ mongoose.Promise = global.Promise;
 
 const SleepSchema = mongoose.Schema({
   //hours: ,
+  date: {type: String, required: true},
   bedTime: {type: Date, required: true},
   awakeTime: {type: Date, default: Date.now, required: true},
   alarm: {type: Boolean, required: true},
@@ -15,13 +16,38 @@ const SleepSchema = mongoose.Schema({
   moodAtSleep: {type: Number, required: true},
 });
 
-// SleepSchema.methods.apiRepr = function() {
-//   return {
-//     username: this.username || '',
-//     firstName: this.firstName || '',
-//     lastName: this.lastName || ''
-//   };
-// };
+SleepSchema.virtual('hours').get(function() {
+  return Math.abs(this.awakeTime - this.bedTime)/36e5;
+});
+
+SleepSchema.virtual('bedTimeMilitary').get(function() {
+  let date = new Date(this.bedTime);
+  let hours = date.getHours();
+  let minutes = '0'+date.getMinutes();
+  return hours+':'+minutes.substr(-2);
+});
+
+SleepSchema.virtual('awakeTimeMilitary').get(function() {
+  let date = new Date(this.awakeTime);
+  let hours = date.getHours();
+  let minutes = '0'+date.getMinutes();
+  return hours+':'+minutes.substr(-2);
+});
+
+SleepSchema.methods.apiRepr = function() {
+  return {
+    hours: this.hours,
+    date: this.date,
+    bedTime: this.bedTimeMilitary,
+    awakeTime: this.awakeTimeMilitary,
+    alarm: this.alarm,
+    exercise: this.exercise,
+    blueLight: this.blueLight,
+    caffeine: this.caffeine,
+    moodAtWake: this.moodAtWake,
+    moodAtSleep: this.moodAtSleep
+  };
+};
 
 const Sleep = mongoose.model('Sleep', SleepSchema);
 
--- sleeps/sleeps.router.js
+++ sleeps/sleeps.router.js
@@ -12,11 +12,55 @@ const authenticate = passport.authenticate('jwt', {session: false});
 
 router.get('/', (req, res) => {
   return Sleep.find()
-    .then(sleeps => res.json(sleeps.map(sleep => sleep)))
+    .then(sleeps => {
+      res.json(
+        sleeps.map(
+          (sleep) => sleep.apiRepr())
+      );
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({message: 'Internal server error'});
+    });
+});
+
+router.delete('/:date', (req, res) => {
+  console.log(req.params.date);
+  Sleep
+    .findOneAndRemove({date: req.params.date})
+    .then(() => res.status(204).end())
     .catch(err => res.status(500).json({message: 'Internal server error'}));
 });
 
+router.post('/new', jsonParser, (req, res) => {
+  const requiredFields = ['date', 'bedTime', 'awakeTime', 'alarm', 'exercise', 'blueLight', 'caffeine', 'moodAtWake', 'moodAtSleep'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing ${field} in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  Sleep
+    .create({
+      date: req.body.date,
+      bedTime: req.body.bedTime,
+      awakeTime: req.body.awakeTime,
+      alarm: req.body.alarm,
+      exercise: req.body.exercise,
+      blueLight: req.body.blueLight,
+      caffeine: req.body.caffeine,
+      moodAtWake: req.body.moodAtWake,
+      moodAtSleep: req.body.moodAtSleep})
+    .then(
+      sleep => res.status(201).json(sleep.apiRepr()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({message: 'Internal server error'});
+    });
+});
+
 //Will need post and delete and such
-//Reference BlogMongooseAPI
 
 module.exports = router;
