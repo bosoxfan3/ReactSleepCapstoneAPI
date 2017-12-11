79e7fce6340bd28d5eb3cb7d713c4be6c9a0f5e7
Daniel Acquesta <danielacquesta@sbcglobal.net>
2017-12-11 14:28:00 -0800

    added date virtual to sleep model

--- sleeps/models.js
+++ sleeps/models.js
@@ -4,8 +4,6 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 const SleepSchema = mongoose.Schema({
-  //hours: ,
-  // date: {type: String, required: true},
   bedTime: {type: Date, required: true},
   awakeTime: {type: Date, default: Date.now, required: true},
   alarm: {type: Boolean, required: true},
@@ -34,11 +32,17 @@ SleepSchema.virtual('awakeTimeMilitary').get(function() {
   return hours+':'+minutes.substr(-2);
 });
 
+SleepSchema.virtual('date').get(function() {
+  let date = new Date(this.bedTime);
+  let dateString = String(date);
+  return dateString.slice(4, 15);
+});
+
 SleepSchema.methods.apiRepr = function() {
   return {
     id: this._id,
     hours: this.hours,
-    // date: this.date,
+    date: this.date,
     bedTime: this.bedTimeMilitary,
     awakeTime: this.awakeTimeMilitary,
     alarm: this.alarm,
