11088ba5e441dba73b75f6de1aa8739ab69182ec
Daniel Acquesta <danielacquesta@sbcglobal.net>
2017-12-19 12:36:42 -0800

    updated sleep router

--- sleeps/router.js
+++ sleeps/router.js
@@ -11,89 +11,91 @@ const router = express.Router();
 //Remove when testing in postman and then add again
 
 router.get('/', (req, res) => {
-  console.log(req.user);
-  return Sleep.find({user: req.user.id})
-  .then(sleeps => {
-    res.json(
-      sleeps.map(
-        (sleep) => sleep.apiRepr())
+  return Sleep
+    .find({user: req.user.id})
+    .then(sleeps => {
+      res.json(
+        sleeps.map(
+          (sleep) => sleep.apiRepr())
       );
     })
     .catch(err => {
       console.error(err);
       res.status(500).json({message: 'Internal server error'});
     });
-  });
-  router.get('/:id', (req, res) => {
-    return Sleep.findOne({_id: req.params.id, user: req.user.id})
+});
+
+router.get('/:id', (req, res) => {
+  return Sleep
+    .findOne({_id: req.params.id, user: req.user.id})
     .then(sleep => {
-      res.json(sleep.apiRepr())
+      res.json(sleep.apiRepr());
     })
     .catch(err => {
       console.error(err);
       res.status(500).json({message: 'Internal server error'});
     });
-  });
+});
 
 
-  router.delete('/:id', (req, res) => {
-    console.log(req.params.id);
-    Sleep
+router.delete('/:id', (req, res) => {
+  console.log(req.params.id);
+  Sleep
     .findOneAndRemove({_id: req.params.id})
     .then(() => res.status(204).end())
     .catch(err => res.status(500).json({message: 'Internal server error'}));
-  });
+});
 
-  router.put('/:id', (req, res) => {
-    console.log(req.params.id);
-    if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
-      const message = (
-        `Request path id (${req.params.id}) and request body id` +
+router.put('/:id', (req, res) => {
+  console.log(req.params.id);
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id` +
         `(${req.body.id}) must match`);
-        console.error(message);
-        return res.status(400).json({message: message});
-      }
-      const toUpdate = {};
-      const updateableFields = ['bedTime', 'awakeTime', 'alarm', 'exercise', 'blueLight', 'caffeine', 'moodAtWake', 'moodAtSleep'];
-      updateableFields.forEach(field => {
-        if (field in req.body) {
-          toUpdate[field] = req.body[field];
-        }
-      });
-      Sleep
-      .findOneAndUpdate({_id: req.params.id}, {$set: toUpdate})
-      .then(sleep => res.status(204).end())
-      .catch(err => res.status(500).json({message: 'Internal server error'}));
+    console.error(message);
+    return res.status(400).json({message: message});
+  }
+  const toUpdate = {};
+  const updateableFields = ['bedTime', 'awakeTime', 'alarm', 'exercise', 'blueLight', 'caffeine', 'moodAtWake', 'moodAtSleep'];
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      toUpdate[field] = req.body[field];
+    }
+  });
+  Sleep
+    .findOneAndUpdate({_id: req.params.id}, {$set: toUpdate})
+    .then(sleep => res.status(204).end())
+    .catch(err => res.status(500).json({message: 'Internal server error'}));
+});
+
+router.post('/', (req, res) => {
+  console.log('POST', req.body);
+  const requiredFields = ['bedTime', 'awakeTime', 'alarm', 'exercise', 'blueLight', 'caffeine', 'moodAtWake', 'moodAtSleep'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing ${field} in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  Sleep
+    .create({
+      user: req.user.id,
+      bedTime: req.body.bedTime,
+      awakeTime: req.body.awakeTime,
+      alarm: req.body.alarm,
+      exercise: req.body.exercise,
+      blueLight: req.body.blueLight,
+      caffeine: req.body.caffeine,
+      moodAtWake: req.body.moodAtWake,
+      moodAtSleep: req.body.moodAtSleep})
+    .then(
+      sleep => res.status(201).json(sleep.apiRepr()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({message: 'Internal server error'});
     });
+});
 
-    router.post('/', (req, res) => {
-      console.log('POST', req.body);
-      const requiredFields = ['bedTime', 'awakeTime', 'alarm', 'exercise', 'blueLight', 'caffeine', 'moodAtWake', 'moodAtSleep'];
-      for (let i=0; i<requiredFields.length; i++) {
-        const field = requiredFields[i];
-        if (!(field in req.body)) {
-          const message = `Missing ${field} in request body`;
-          console.error(message);
-          return res.status(400).send(message);
-        }
-      }
-      Sleep
-      .create({
-        user: req.user.id,
-        bedTime: req.body.bedTime,
-        awakeTime: req.body.awakeTime,
-        alarm: req.body.alarm,
-        exercise: req.body.exercise,
-        blueLight: req.body.blueLight,
-        caffeine: req.body.caffeine,
-        moodAtWake: req.body.moodAtWake,
-        moodAtSleep: req.body.moodAtSleep})
-        .then(
-          sleep => res.status(201).json(sleep.apiRepr()))
-          .catch(err => {
-            console.error(err);
-            res.status(500).json({message: 'Internal server error'});
-          });
-        });
-
-        module.exports = router;
+module.exports = router;
